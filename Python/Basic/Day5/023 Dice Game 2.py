fc62e55cf5f2ce1da4816ea4de3d6547af520f04
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2026-01-29 22:19:03 +0900

    Add files via upload

---
+++ Python/Basic/Day5/023 Dice Game 2.py
@@ -0,0 +1,11 @@
+def solution(a, b, c):
+    answer = 0
+    
+    if a != b and a != c and b != c:
+        answer = a + b + c
+    elif (a == b and a != c and b != c) or (a != b and a == c and b != c) or (a != b and a != c and b == c):
+        answer = (a + b + c) * ((a * a) + (b * b) + (c * c))
+    elif a == b and b == c:
+        answer = (a + b + c) * ((a * a) + (b * b) + (c * c)) * ((a * a * a) + (b * b * b) + (c * c * c))
+    
+    return answer
